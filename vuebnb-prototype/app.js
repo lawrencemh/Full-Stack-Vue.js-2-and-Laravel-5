062adeeec5e4682256e19bec14bbc4efac47524c
lawrencemh <lawrencemh@outlook.com>
2018-01-31 21:05:15 +0000

    WIP Complete chapter 1.

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,13 @@
+var app = new Vue({
+    el  : '#app',
+    data: {
+        title           : sample.title,
+        address         : sample.address,
+        about           : sample.about,
+        amenities       : sample.amenities,
+        prices          : sample.prices,
+        headerImageStyle: {
+            'background-image': 'url(sample/header.jpg)'
+        }
+    }
+});
