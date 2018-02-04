0dfc8aa8e873e8c17e2f143b314876e1d9f54b00
lawrencemh <lawrencemh@outlook.com>
2018-02-04 14:56:27 +0000

    Update package.json, webpack and add yarn lock file.
    * Removed unused dependencies form package.json including jQuery, bootstrap-sass and lodash.
    * Add browser-sync, font-awesome, core-js, dotenv and open-sans-all dependencies.

--- vuebnb/webpack.mix.js
+++ vuebnb/webpack.mix.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 let mix = require('laravel-mix');
 
 /*
@@ -12,4 +13,9 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .copy('resources/assets/images', 'public/images')
+    .browserSync({
+        proxy: process.env.APP_URL,
+        open : false
+    });
